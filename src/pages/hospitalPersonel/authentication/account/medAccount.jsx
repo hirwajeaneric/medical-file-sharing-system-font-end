bd3c893a9af8f7acbe25f6fce7a457fa354fb57d
hirwajeaneric <hirwajeaneric25@gmail.com>
2022-12-13 16:46:21 +0200

    First commit

---
+++ src/pages/hospitalPersonel/authentication/account/medAccount.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const medAccount = () => {
+  return (
+    <div>medAccount</div>
+  )
+}
+
+export default medAccount
---
+++ src/pages/patient/patientAccount/patientAccount.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const patientAccount = () => {
+  return (
+    <div>patientAccount</div>
+  )
+}
+
+export default patientAccount
